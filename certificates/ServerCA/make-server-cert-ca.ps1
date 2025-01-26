52d08a43609bee14477c86185e54a2c0ecea4de4
juniordevops <lukasz.tomalczyk@outlook.com>
2025-01-26 10:19:05 +0100

    breake lines

--- certificates/ServerCA/make-server-cert-ca.ps1
+++ certificates/ServerCA/make-server-cert-ca.ps1
@@ -5,7 +5,7 @@ $subject = "CN=$domain, O=SpCraft"
 $friendlyName = "SpCraft.FileUploader Server Certificate"
 $password = ConvertTo-SecureString -String "tajnehaslo123" -AsPlainText -Force
 $pfxPath = Join-Path -Path $PSScriptRoot -ChildPath "$domain.pfx"
-$caCertThumbprint = "7b3586b538f534ec412c5238383bd3efbf205967"  # Wprowadź tutaj thumbprint certyfikatu Intermediate CA
+$caCertThumbprint = "e4fe30a3f11fbabc6ea4498652b545948b044f87"  # Wprowadź tutaj thumbprint certyfikatu Intermediate CA
 
 # Znalezienie certyfikatu Intermediate CA
 $caCert = Get-ChildItem -Path "cert:\LocalMachine\CA" | Where-Object { $_.Thumbprint -eq $caCertThumbprint }
@@ -27,8 +27,21 @@ Write-Host "Intermediate CA Certificate Thumbprint: $($caCertObject.Thumbprint)"
 Write-Host "Intermediate CA Certificate Subject: $($caCertObject.Subject)"
 
 # Utworzenie certyfikatu serwera
-$cert = New-SelfSignedCertificate -CertStoreLocation "cert:\LocalMachine\My" -KeyLength 2048 -KeyExportPolicy Exportable -NotAfter (Get-Date).AddYears(1) -Signer $caCertObject -Subject $subject -FriendlyName $friendlyName -KeyUsage DigitalSignature, KeyEncipherment -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1", "2.5.29.17={text}$san", "2.5.29.19={text}CA=false")
-
+$cert = New-SelfSignedCertificate `
+    -CertStoreLocation "cert:\LocalMachine\My" `
+    -KeyLength 2048 `
+    -KeyExportPolicy Exportable `
+    -NotAfter (Get-Date).AddYears(1) `
+    -Signer $caCertObject `
+    -Subject $subject `
+    -FriendlyName $friendlyName `
+    -KeyUsage DigitalSignature, KeyEncipherment `
+    -TextExtension @(
+        "2.5.29.37={text}1.3.6.1.5.5.7.3.1", 
+        "2.5.29.17={text}$san", 
+        "2.5.29.19={text}CA=false"
+    )
+    
 # Eksportowanie certyfikatu do pliku PFX
 Export-PfxCertificate -Cert $cert -FilePath $pfxPath -Password $password
 
